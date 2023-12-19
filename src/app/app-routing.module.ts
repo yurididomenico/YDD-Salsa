bb5b11fcbebcfcafe13602a9a526b4f4db60884d
Yuri DI DOMENICO <y.didomenico@cineca.it>
2023-12-19 13:43:08 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TestPageComponent } from './pages/test-page/test-page.component';
+import { TestPage2Component } from './pages/test-page2/test-page2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'page1', component: TestPageComponent},
+  {path: 'page2', component: TestPage2Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
